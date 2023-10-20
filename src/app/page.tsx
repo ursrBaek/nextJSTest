b88a872a02aa02f640b83ad4241b0d1c7106edb6
ursrBaek <ursrBaek@gmail.com>
2023-10-20 16:40:13 +0900

    style 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
+import style from './style.module.css'
+
 export default function Home() {
   return (
     <>
-      <h1>main</h1>
-      <ul>
+      <h1>날씨 앱</h1>
+      <ul className={style.list}>
         <li><Link href='/seoul'>서울</Link></li>
         <li><Link href='/newyork'>뉴욕</Link></li>
         <li><Link href='/london'>런던</Link></li>
