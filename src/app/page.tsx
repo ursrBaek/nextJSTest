573707476d5b1daf6d0a1a0c317c7d480adbd353
ursrBaek <ursrBaek@gmail.com>
2023-10-20 17:29:36 +0900

    날씨 데이터 조회로직 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 
 import style from './style.module.css'
+import { getCurrentWeather } from "./utils/getCurrentWeather";
 
-export default function Home() {
+
+
+export default async function Home() {
+  const res = await getCurrentWeather('Seoul');
+
   return (
     <>
       <h1>날씨 앱</h1>
       <ul className={style.list}>
-        <li><Link href='/seoul'>서울</Link></li>
+        <li>
+          <Link href='/seoul'>서울</Link>
+          <span>{res.current.condition.text}</span>
+        </li>
         <li><Link href='/newyork'>뉴욕</Link></li>
         <li><Link href='/london'>런던</Link></li>
       </ul>
