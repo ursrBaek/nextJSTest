4970b79d55021c30e368b9afb64ef8ba47177c5a
ursrBaek <ursrBaek@gmail.com>
2023-10-21 09:33:47 +0900

    fetch 로직 수정(테스트3)

--- src/app/utils/getCurrentWeather.ts
+++ src/app/utils/getCurrentWeather.ts
@@ -50,10 +50,10 @@ export interface Condition {
 
 
 export const getCurrentWeather = async (location: string):Promise<Response> => {
-  const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${location}&aqi=no`, {next: {tags: ['weather']}});
+  const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=b76ed647567a45c991774225232010&q=${location}&aqi=no`, {next: {tags: ['weather']}});
 
   if (!res.ok) {
-    console.log(`http://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${location}&aqi=no`);
+    console.log(`http://api.weatherapi.com/v1/current.json?key=b76ed647567a45c991774225232010&q=${location}&aqi=no`);
     throw new Error('날씨 정보를 가져올 수 없습니다!');
   }
 
